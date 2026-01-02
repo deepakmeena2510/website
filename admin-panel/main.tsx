4b27c886a6d47a4e9003c0d1c916774fe0ce8192
deepakmeena2510 <deepakmeena946090@gmail.com>
2026-01-02 21:09:44 +0530

    Create main.tsx

---
+++ admin-panel/main.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import Reels from "./pages/Reels";
+import Profile from "./pages/Profile";
+
+function App() {
+  return (
+    <Routes>
+      {/* Default route */}
+      <Route path="/" element={<Navigate to="/login" />} />
+
+      {/* Auth pages */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+
+      {/* Main pages */}
+      <Route path="/home" element={<Home />} />
+      <Route path="/reels" element={<Reels />} />
+      <Route path="/profile" element={<Profile />} />
+
+      {/* 404 */}
+      <Route path="*" element={<h2>पेज नहीं मिला</h2>} />
+    </Routes>
+  );
+}
+
+export default App;
