1eef5265525d0b7dc0954640adf16f221efbf3c9
deepakmeena2510 <deepakmeena946090@gmail.com>
2026-01-02 21:31:09 +0530

    Create Signup.tsx

---
+++ instagram-clone-ap/src/pages/Signup.tsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+export default function Signup() {
+  return (
+    <div>
+      <h2>Signup</h2>
+      <Link to="/login">Go to Login</Link>
+    </div>
+  );
+}
