c3d6f248f1790939746d3ea8d40e0a8a1bc926d0
deepakmeena2510 <deepakmeena946090@gmail.com>
2026-01-03 10:44:43 +0530

    Update App.tsx

--- instagram-clone-ap/src/App.tsx
+++ instagram-clone-ap/src/App.tsx
@@ -1,9 +1,19 @@
-import Home from './pages/Home'
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import Reels from "./pages/Reels";
+import Profile from "./pages/Profile";
 
 export default function App() {
   return (
-    <div style={{ fontFamily: 'Arial', padding: 20 }}>
-      <Home />
-    </div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/reels" element={<Reels />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  );
+} 
