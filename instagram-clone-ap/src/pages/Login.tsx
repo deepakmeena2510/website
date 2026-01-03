e28a5e7347f42ce7b460c4a0af8126eb37ca719d
deepakmeena2510 <deepakmeena946090@gmail.com>
2026-01-03 10:31:57 +0530

    Update Login.tsx

--- instagram-clone-ap/src/pages/Login.tsx
+++ instagram-clone-ap/src/pages/Login.tsx
@@ -1,11 +1,32 @@
 import { Link } from "react-router-dom";
+
 export default function Login() {
+  याद
   return (
-    <div>
+    <div style={{ textAlign: "center", marginTop: 60 }}>
       <h2>Login</h2>
-      <Link to="/home">Login</Link>
+
+      <input
+        type="text"
+        placeholder="Username"
+        style={{ padding: 8, marginBottom: 10 }}
+      />
       <br />
-      <Link to="/signup">Signup</Link>
+
+      <input
+        type="password"
+        placeholder="Password"
+        style={{ padding: 8, marginBottom: 10 }}
+      />
+      <br />
+
+      <Link to="/home">
+        <button style={{ padding: "8px 20px" }}>Login</button>
+      </Link>
+
+      <p style={{ marginTop: 10 }}>
+        New user? <Link to="/signup">Signup</Link>
+      </p>
     </div>
   );
 }
