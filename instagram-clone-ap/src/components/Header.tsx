c1e6fd5bb7a3f4650285a348906cb29eb97cbc2a
deepakmeena2510 <deepakmeena946090@gmail.com>
2026-01-03 10:49:55 +0530

    Update Header.tsx

--- instagram-clone-ap/src/components/Header.tsx
+++ instagram-clone-ap/src/components/Header.tsx
@@ -2,13 +2,12 @@ export default function Header() {
   return (
     <div style={{
       padding: 15,
-      textAlign: 'center',
-      fontWeight: 'bold',
+      textAlign: "center",
       fontSize: 22,
-      borderBottom: '1px solid #ddd',
-      background: '#fff'
+      fontWeight: "bold",
+      borderBottom: "1px solid #ddd"
     }}>
       Instagram Clone
     </div>
-  )
+  );
 }
