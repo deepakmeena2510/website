6dae801bcf04a7299503bb14de53c8d68c207f30
deepakmeena2510 <deepakmeena946090@gmail.com>
2026-01-03 16:07:52 +0530

    Delete instagram-clone-ap directory

--- instagram-clone-ap/src/pages/Profile.tsx
+++
@@ -1,9 +0,0 @@
-export default function Profile() {
-  return (
-    <div style={{ textAlign: "center", marginTop: 60 }}>
-      <h2>Profile</h2>
-      <p>Username: demo_user</p>
-      <p>Posts: 0</p>
-    </div>
-  );
-}
