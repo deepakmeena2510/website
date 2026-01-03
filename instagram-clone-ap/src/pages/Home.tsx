8eab8f1c8a7da4120e5662643831f495d40a05ed
deepakmeena2510 <deepakmeena946090@gmail.com>
2026-01-03 10:47:27 +0530

    Update Home.tsx

--- instagram-clone-ap/src/pages/Home.tsx
+++ instagram-clone-ap/src/pages/Home.tsx
@@ -1,11 +1,12 @@
-import Header from '../components/Header'
-import PostCard from '../components/PostCard'
+import Header from "../components/Header";
+import PostCard from "../components/PostCard";
 
 export default function Home() {
   return (
     <>
       <Header />
       <PostCard />
+      <PostCard />
     </>
-  )
+  );
 }
