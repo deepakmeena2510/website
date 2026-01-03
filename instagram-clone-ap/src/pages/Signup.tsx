a63b647dfef45b201a0ad15eb3d4d88c7f9ba30d
deepakmeena2510 <deepakmeena946090@gmail.com>
2026-01-03 10:32:40 +0530

    Update Signup.tsx

--- instagram-clone-ap/src/pages/Signup.tsx
+++ instagram-clone-ap/src/pages/Signup.tsx
@@ -1,9 +1,36 @@
 import { Link } from "react-router-dom";
+
 export default function Signup() {
   return (
-    <div>
+    <div style={{ textAlign: "center", marginTop: 60 }}>
       <h2>Signup</h2>
-      <Link to="/login">Go to Login</Link>
+
+      <input
+        type="text"
+        placeholder="Username"
+        style={{ padding: 8, marginBottom: 10 }}
+      />
+      <br />
+
+      <input
+        type="email"
+        placeholder="Email"
+        style={{ padding: 8, marginBottom: 10 }}
+      />
+      <br />
+
+      <input
+        type="password"
+        placeholder="Password"
+        style={{ padding: 8, marginBottom: 10 }}
+      />
+      <br />
+
+      <button style={{ padding: "8px 20px" }}>Create Account</button>
+
+      <p style={{ marginTop: 10 }}>
+        Already have account? <Link to="/login">Login</Link>
+      </p>
     </div>
   );
 }
