b3f45eef8d6ba6b179e324261c78f78026d4df42
deepakmeena2510 <deepakmeena946090@gmail.com>
2026-01-03 10:16:05 +0530

    Create PostCard.tsx

---
+++ instagram-clone-ap/src/components/PostCard.tsx
@@ -0,0 +1,13 @@
+export default function PostCard() {
+  return (
+    <div style={{
+      maxWidth: 300,
+      margin: '20px auto',
+      padding: 15,
+      border: '1px solid #ccc'
+    }}>
+      <b>demo_user</b>
+      <p>This is a demo post</p>
+    </div>
+  )
+}
