97157b95755c57ee350fd1861ec3115a130ed631
deepakmeena2510 <deepakmeena946090@gmail.com>
2026-01-03 10:12:58 +0530

    Update App.tsx

--- instagram-clone-ap/src/App.tsx
+++ instagram-clone-ap/src/App.tsx
@@ -1,24 +1,9 @@
-function App() {
+import Home from './pages/Home'
+
+export default function App() {
   return (
-    <div style={{
-      minHeight: '100vh',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      fontFamily: 'Arial'
-    }}>
-      <div style={{
-        border: '1px solid #ddd',
-        padding: '30px',
-        borderRadius: '10px',
-        textAlign: 'center'
-      }}>
-        <h1>Instagram Clone</h1>
-        <p>GitHub Pages सही से काम कर रहा है ✅</p>
-        <p>अब React build जुड़ना बाकी है</p>
-      </div>
+    <div style={{ fontFamily: 'Arial', padding: 20 }}>
+      <Home />
     </div>
   )
 }
-
-export default App
