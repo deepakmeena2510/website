b389d6a7e547b46aa2e06857ffb1d73d14a0f4cb
deepakmeena2510 <deepakmeena946090@gmail.com>
2026-01-03 10:46:46 +0530

    Update Signup.tsx

--- instagram-clone-ap/src/pages/Signup.tsx
+++ instagram-clone-ap/src/pages/Signup.tsx
@@ -5,30 +5,13 @@ export default function Signup() {
     <div style={{ textAlign: "center", marginTop: 60 }}>
       <h2>Signup</h2>
 
-      <input
-        type="text"
-        placeholder="Username"
-        style={{ padding: 8, marginBottom: 10 }}
-      />
-      <br />
-
-      <input
-        type="email"
-        placeholder="Email"
-        style={{ padding: 8, marginBottom: 10 }}
-      />
-      <br />
-
-      <input
-        type="password"
-        placeholder="Password"
-        style={{ padding: 8, marginBottom: 10 }}
-      />
-      <br />
+      <input placeholder="Username" style={{ padding: 8 }} /><br /><br />
+      <input placeholder="Email" style={{ padding: 8 }} /><br /><br />
+      <input type="password" placeholder="Password" style={{ padding: 8 }} /><br /><br />
 
       <button style={{ padding: "8px 20px" }}>Create Account</button>
 
-      <p style={{ marginTop: 10 }}>
+      <p>
         Already have account? <Link to="/login">Login</Link>
       </p>
     </div>
