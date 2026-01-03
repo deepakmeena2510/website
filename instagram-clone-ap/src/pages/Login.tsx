795ab1eb71b1e91ae27b2e714bc0856e61414740
deepakmeena2510 <deepakmeena946090@gmail.com>
2026-01-03 10:45:52 +0530

    Update Login.tsx

--- instagram-clone-ap/src/pages/Login.tsx
+++ instagram-clone-ap/src/pages/Login.tsx
@@ -1,30 +1,18 @@
 import { Link } from "react-router-dom";
 
 export default function Login() {
-  याद
   return (
     <div style={{ textAlign: "center", marginTop: 60 }}>
       <h2>Login</h2>
 
-      <input
-        type="text"
-        placeholder="Username"
-        style={{ padding: 8, marginBottom: 10 }}
-      />
-      <br />
-
-      <input
-        type="password"
-        placeholder="Password"
-        style={{ padding: 8, marginBottom: 10 }}
-      />
-      <br />
+      <input placeholder="Username" style={{ padding: 8 }} /><br /><br />
+      <input type="password" placeholder="Password" style={{ padding: 8 }} /><br /><br />
 
       <Link to="/home">
         <button style={{ padding: "8px 20px" }}>Login</button>
       </Link>
 
-      <p style={{ marginTop: 10 }}>
+      <p>
         New user? <Link to="/signup">Signup</Link>
       </p>
     </div>
