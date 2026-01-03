5b4bcc69d98136e738c7eff16ed4823b7ac68a80
deepakmeena2510 <deepakmeena946090@gmail.com>
2026-01-03 10:36:29 +0530

    Update Reels.tsx

--- instagram-clone-ap/src/pages/Reels.tsx
+++ instagram-clone-ap/src/pages/Reels.tsx
@@ -1,3 +1,8 @@
 export default function Reels() {
-  return <h2>Reels Page</h2>;
+  return (
+    <div style={{ textAlign: "center", marginTop: 60 }}>
+      <h2>Reels</h2>
+      <p>Reels feature coming soon...</p>
+    </div>
+  );
 }
